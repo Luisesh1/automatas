d6dea9a4603af9f0e95eccda97fabb77a944a6aa
luisesh11 <luisesh1x10@hotmail.com>
2016-04-25 18:08:01 +0000

    se arreglaron detalles de lex

--- ruby/pruebas.rb
+++ ruby/pruebas.rb
@@ -1,43 +1,59 @@
 
 def scan (cadena)
-      nivel = 1
-      acomulado=""
-      tokens=[[],[]]
-       @tabla = [["1","a","+","=",nil],
-               [  2,  3,  4,  5, nil],
-               [  2, nil,nil,nil,"digito"],
-               [ nil, 3,nil,nil,"letra"],
-               [ nil,nil,nil,nil,"+"],
-               [ nil,nil,nil,6,"="],
-               [ nil,nil,nil,nil,"=="]] 
-        i=0
-        while (i<cadena.length)
-          c= cadena[i]
-          puts "nivel #{nivel}"
-          puts "nivel #{pos(c)}"
-          nivel = @tabla[nivel][pos(c)]
-          if nivel.nil? 
-            puts acomulado 
-            puts @tabla[2][5]
-            tokens.push([acomulado],[])
-            acomulado=""
-            puts tokens
-          end
+  nivel = 1
+  acomulado=""
+  tokens=[[],[]]
+   @tabla = [["1","a","+","=",nil],
+           [  2,  3,  4,  5, nil],
+           [  2, nil,nil,nil,"digito"],
+           [ nil, 3,nil,nil,"letra"],
+           [ nil,nil,nil,nil,"+"],
+           [ nil,nil,nil,6,"="],
+           [ nil,nil,nil,nil,"=="]] 
+    i=0
+    while (i<cadena.length)
+      c= cadena[i]
+      unless isValido(c)
+          puts "se encontro caracter invalido"
+          return
+      end
+      puts "nivel #{nivel}"
+      puts "col #{pos(c)}"
+      if @tabla[nivel][pos(c)].nil? 
+        #puts acomulado 
+        #puts @tabla[2][5]
+        tokens.push(acomulado,@tabla[nivel][@tabla[nivel].length-1])
+        acomulado=""
+        puts "toquen encontrado"
+        nivel=1
+      else
+          nivel=@tabla[nivel][pos(c)]
           i+=1
           acomulado+=c
-        end
- 
+      end
+    end
+    tokens.push(acomulado,@tabla[nivel][@tabla[nivel].length-1])
+    puts tokens
 end
  def pos(c)
-          posi=0
-          col = @tabla.length-1
-          @tabla[0].each do |l|
-            if l==c
-              col=posi 
-            end
-            posi+=1
-          end
-          col
+  posi=0
+  col = @tabla.length-1
+  @tabla[0].each do |l|
+    if l==c
+      col=posi 
+    end
+    posi+=1
+  end
+  col
+end
+def isValido(l)
+    @tabla[0].each do |c|
+        if c==l
+            return true
         end
+    end
+    return false
+end
 
-    scan ("111j11")
+    scan ("111aaaa11111111aaaa+===aaaa")
+    #puts pos("1")
